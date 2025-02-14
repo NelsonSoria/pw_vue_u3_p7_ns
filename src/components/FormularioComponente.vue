a8b07a647d804d6bf3c360615e7af624dfd11b0b
Nelson Soria <nelson-soria@hotmail.com>
2025-02-14 18:33:35 -0500

    Tarea13

--- src/components/FormularioComponente.vue
+++ src/components/FormularioComponente.vue
@@ -1,13 +1,28 @@
 <template>
-    <h1>Hola soy  Persona Componente</h1>
+  <div class="container">
+    <div class="container-formulario">
+      <p type="Nombre:"><input type="text" /></p>
+      <p type="Fecha Nacimiento:"><input type="date" /></p>
+      <p type="Apellido:"><input type="text" /></p>
+    </div>
+  </div>
 </template>
 
 <script>
-import {  obtenerPorIdFachada  } from "@/client/PersonaClient.js";
-export default {
-}
+import { obtenerPorIdFachada } from "@/client/PersonaClient.js";
+export default {};
 </script>
 
 <style>
-
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+ 
+}
+p::before {
+  display: block;
+  content: attr(type);
+  font-weight: bold;
+}
 </style>
